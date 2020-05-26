fcfd6148061591a1b71bbb93c06dc92d105c0afa
Vadim <svanovivan@gmail.com>
2020-05-26 17:52:16 +0300

    Add apiFiveDays

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,8 @@
 import '../styles/styles.css';
 import '../../node_modules/basiclightbox/dist/basicLightbox.min.css';
 import * as basicLightbox from 'basiclightbox';
-import weatherTemplate from '../templates/weather_brodcast-template.hbs';
-import apiService from './services/apiService.js';
+import weatherTemplate from '../templates/weather_forecast-template.hbs';
+import apiServiceOneDay from './services/apiServiceOneDay.js';
 import '@pnotify/core/dist/BrightTheme.css';
 import { alert, error } from '@pnotify/core/dist/PNotify';
 import '@pnotify/core/dist/PNotify.css';
@@ -13,23 +13,28 @@ import _ from 'lodash';
 const refs = {
   input: document.getElementById('search-input'),
   currentWeather: document.getElementById('weather-info-js'),
+  weatherMainTemp: document.querySelector('.weather-main-temp__sp'),
+  buttonOneDay: document.querySelector('[data-action="oneDaysForcast"]'),
+  buttonFiveDay: document.querySelector('[data-action="5daysForcast"]'),
 };
 
 refs.input.addEventListener('input', _.debounce(handleInputValue, 1000));
 refs.currentWeather.style.display = 'none';
+refs.buttonFiveDay.addEventListener('click', handleButtonClickFiveDays);
 
 function handleInputValue(e) {
   e.preventDefault();
   clearMarkup();
 
   const searchQuery = e.target.value;
-  apiService.searchQuery = searchQuery;
+  apiServiceOneDay.searchQuery = searchQuery;
   e.target.value = '';
   createWeatherTemplate();
+  refs.buttonOneDay.classList.add('unactive');
 }
 
 function createWeatherTemplate() {
-  apiService
+  apiServiceOneDay
     .fetchImages()
     .then(buildWeatherItemsMarkup)
     .catch(error => {
@@ -44,7 +49,6 @@ function buildWeatherItemsMarkup(items) {
     refs.currentWeather.style.display = 'flex';
     const markup = weatherTemplate(items);
     refs.currentWeather.insertAdjacentHTML('beforeend', markup);
-    console.log(markup);
   } else {
     refs.currentWeather.style.display = 'none';
     error('No such image found');
@@ -53,4 +57,10 @@ function buildWeatherItemsMarkup(items) {
 
 function clearMarkup() {
   refs.currentWeather.innerHTML = '';
+}
+
+function handleButtonClickFiveDays() {
+  refs.currentWeather.style.display = 'none';
+  createFiveDayWeatherTemplate();
+  console.log(apiServiceOneDay.searchQuery);
 }
---
+++ src/js/services/apiServiceOneDay.js
@@ -0,0 +1,26 @@
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?';
+
+export default {
+  API_KEY: 'e6a412f77abd301d1f3f9864e669e17e',
+
+  query: '',
+  fetchImages() {
+    const requestParams = `q=${this.query}&units=metric&appid=${this.API_KEY}`;
+
+    return fetch(baseUrl + requestParams)
+      .then(response => {
+        return response.json();
+      })
+
+      .catch(error => {
+        throw error;
+      });
+  },
+
+  set searchQuery(string) {
+    this.query = string;
+  },
+  get searchQuery() {
+    return this.query;
+  },
+};
